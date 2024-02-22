f3e6f42be111da886f2dd81c3557bbdbd66c68ea
tomagudelo@gmail.com <you@example.com>
2024-02-22 17:42:22 -0500

    iniciando

---
+++ src/controllers/controlingreso.js
@@ -0,0 +1,55 @@
+//DECLARANDO VARIABLES CON JS DESDE EL DOM
+
+
+let usuarioBaseDatos="sura45"
+let contrasenaBaseDatos="12345"
+let correoBaseDatos="savelo@gmail.com"
+
+let botonFormulario=document.getElementById("botonIngreso")
+let cajaFormularioEmail=document.getElementById("email")
+let cajaFormularioNombre=document.getElementById("nombre")
+let cajaFormularioPassword=document.getElementById("password")
+
+//DETECTANDO EVENTOS CON JS
+botonFormulario.addEventListener("click",function(evento){
+
+    evento.preventDefault()
+
+   //1.capturamos la informacion del formulario
+   let nombreUsuario=cajaFormularioNombre.value  
+   let correoUsuario=cajaFormularioEmail.value 
+   let passwordUsuario=cajaFormularioPassword.value
+
+
+   //Validando los datos del usuario
+   if(usuarioBaseDatos==nombreUsuario){
+    Swal.fire({
+        title: "Bienvenido "+nombreUsuario,
+        text: "Tus credenciales son correctas",
+        icon: "success"
+      })
+
+      window.location.href="./src/views/home.html"
+
+   }else{
+        Swal.fire({
+        icon: "error",
+        title: "Oops..."+nombreUsuario+ "tienes problemas",
+        text: "Verifica tus credenciales",
+        
+      });
+
+   }
+  
+
+
+})
+
+cajaFormularioEmail.addEventListener("click", function(){alert("digite su Email")})
+cajaFormularioNombre.addEventListener("click", function(){alert("digite su nombre")})
+cajaFormularioPassword.addEventListener("click", function(){alert("digite su contraseña")})
+
+/*cajaFormularioEmail.addEventListener("click", function(){alert("digite su Email")})
+cajaFormularioNombre.addEventListener("click", function(){alert("digite su nombre")})
+cajaFormularioPassword.addEventListener("click", function(){alert("digite su contraseña")})
+*/
